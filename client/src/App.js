5d719b2924af20d5a97344c0e3661368a2344552
Myat Soe <myatsoe.sbs@gmail.com>
2022-12-26 09:53:54 +0630

    adding files

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+// import { Button } from "antd";
+import "./stylesheets/theme.css";
+import "./stylesheets/theme.css";
+import "./stylesheets/alignments.css";
+import "./stylesheets/textelements.css";
+function App() {
+  return (
+    <div className="App">
+     <div className='h-screen bg-primary'> </div>
+    </div>
+  );
+}
+
+export default App;
